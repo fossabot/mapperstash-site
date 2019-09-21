ba520f3a4ca906a9fc03a2324b67598870d8979b
deekts <taplejung@protonmail.ch>
2019-09-21 20:56:14 +0000

    minor fix
    Added a missing semicolon and some forward-slashes

--- public/index.php
+++ public/index.php
@@ -5,10 +5,10 @@ $request = explode('/', $_SERVER['REQUEST_URI']);
 switch ($request[1]) {
 	case '':
 		# This is the homepage
-		include_once $_SERVER['DOCUMENT_ROOT'].'../parts/head.php'
-		include_once $_SERVER['DOCUMENT_ROOT'].'../parts/header.php';
-		include_once $_SERVER['DOCUMENT_ROOT'].'../parts/homepage.php';
-		include_once $_SERVER['DOCUMENT_ROOT'].'../parts/footer.php';
+		include_once $_SERVER['DOCUMENT_ROOT'].'/../parts/head.php';
+		include_once $_SERVER['DOCUMENT_ROOT'].'/../parts/header.php';
+		include_once $_SERVER['DOCUMENT_ROOT'].'/../parts/homepage.php';
+		include_once $_SERVER['DOCUMENT_ROOT'].'/../parts/footer.php';
 		break;
 	case 'about':
 		echo 'two';
