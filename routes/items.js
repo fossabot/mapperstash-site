43ab423b1c4fe58ef7b8ad104c8dca6a80f60fad
Aditya <taplejung@protonmail.ch>
2020-08-16 15:10:39 +0100

    Implement page search

--- routes/items.js
+++ routes/items.js
@@ -6,33 +6,57 @@ const config = require('./../config.json')
 router.get('/items', async (req, res) => {
   const ItemModel = require('../models/items.js')
   const TagModel = require('../models/tags.js')
-  var includes, excludes
-  var querytags = req.query.tags.split('+')
+  var querytags, includes, excludes
+  if (req.query.tags) {
+    querytags = req.query.tags.split(' ')
 
-  if (querytags.some(querytag => querytag.startsWith('-'))) {
-    excludes = querytags.filter(querytag => querytag.startsWith('-'))
-    querytags = querytags.filter(querytag => !(querytag.startsWith('-')))
+    if (querytags.some(querytag => querytag.startsWith('-'))) {
+      excludes = querytags.filter(querytag => querytag.startsWith('-'))
+      excludes = excludes.map(exclude => {return exclude.substring(1)})
+      querytags = querytags.filter(querytag => !(querytag.startsWith('-')))
+    }
+    if (querytags.length) {
+      includes = querytags
+    }
+
+    if (includes) {
+      includes = await Promise.all(includes.map(async include => {
+        return TagModel.findOne({ tag: include }).exec()
+      }))
+      includes = includes.map(include => {return include.id})
+    }
+
+    if (excludes) {
+      excludes = await Promise.all(excludes.map(async exclude => {
+        return TagModel.findOne({ tag: exclude }).exec()
+      }))
+      excludes = excludes.map(exclude => {return exclude.id})
+    }
   }
-  if (querytags.length) includes = querytags
 
-  var countquery = ItemModel.countDocuments({})
+  var countquery = ItemModel.countDocuments()
 
-  if (req.query.page) {
-    
-  }
-  /*
-  const itemcount = await db.collection('items').countDocuments(queryobj)
-  const pages = Math.ceil(itemcount / 10)
+  if (includes) countquery.where('tags').all(includes)
+  if (excludes) countquery.where('tags').nin(excludes)
+
+  const itemcount = await countquery.exec()
+  const pagesize = 10
+  const pages = Math.ceil(itemcount / pagesize)
+
+  var pagequery = ItemModel.find()
 
   if (req.query.page) {
     if (!(Number.isInteger(Number(req.query.page))) || req.query.page < 1) return res.send("Error")
-    query += `.skip(${(req.query.page - 1) * 10})`
+    pagequery.skip((req.query.page - 1) * pagesize)
   }
-  query += '.limit(10).toArray()'
+  pagequery.limit(pagesize)
 
-  const results = (await eval(`db.collection('items')${query}`)
-  */
-  res.render('items.ejs', {items:[{name:'a',url:'#',link:'#',tags:['a']}],lastpage:2,currentpage:1})
+  if (includes) pagequery.where('tags').all(includes)
+  if (excludes) pagequery.where('tags').nin(excludes)
+
+  var pageresults = await pagequery.exec()
+
+  res.render('items.ejs', {items: pageresults, lastpage: pages, currentpage: itemcount})
 })
 
 module.exports = router
